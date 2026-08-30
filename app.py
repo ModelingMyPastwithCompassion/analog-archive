5be51b408fff32aa59ca94d55a6f657390dfe1be
ModelingMyPastwithCompassion <yaronbarzilai66@gmail.com>
2026-08-30 10:34:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ elif st.session_state.human_status == "yes":
     st.title("Modeling My Past with Compassion")
     st.markdown("""
     <div style="text-align: center; position: relative; z-index: 10;">
-    <em>This engine is a personal archive, constructed from fragments of VHS tapes shot by my parents throughout the 90s. The cinematography belongs to them. Built as a closed, independent protocol, free from commercial extraction—it merges this analog footage with synthetic processes to simulate how human memory actually works: distorting, adding or subtracting details, and scrambling the timeline. Is this how it really happened, or is this simply how I choose to remember it?</em><br><br>
+    <em>This engine is a personal archive, constructed from fragments of VHS tapes shot by my parents throughout the 90s. The cinematography belongs to them. Built as a closed, independent protocol, free from commercial extraction, it merges this analog footage with synthetic processes to simulate how human memory actually works: distorting, adding or subtracting details, and scrambling the timeline. Is this how it really happened, or is this simply how I choose to remember it?</em><br><br>
     <strong>Please treat these materials with compassion.</strong>
     </div>
     <br>
